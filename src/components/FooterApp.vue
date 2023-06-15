de08d6d8174e44d64f8f0b3d1b12c3cafffb1152
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-15 16:05:04 +0200

    footer + font

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,5 +1,12 @@
 <template>
-    <p>Sono il footer</p>
+    <footer class="d-flex align-items-center justify-content-center flex-column">
+        <p class="text-white">Made with &hearts; by Konoha Team</p>
+        <div class="d-flex">
+            <p class="text-white me-2">Take a look to our repo:</p>
+            <a href="https://github.com/itschiaras/vue-game-creator"><i class="fa-brands fa-github"></i></a>
+        </div>
+
+    </footer>
 </template>
 
 <script>
@@ -9,5 +16,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+footer {
+    display: block;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 100px;
+    background: rgba(0, 0, 0, 0.28);
+    backdrop-filter: blur(4.5px);
+    -webkit-backdrop-filter: blur(4.5px);
+}
 </style>
