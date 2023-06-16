d098333ad86baee920e88d7425f5563398088c69
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-16 12:03:47 +0200

    init template characters list

--- src/assets/pages/CharactersList.vue
+++ src/assets/pages/CharactersList.vue
@@ -1,12 +1,44 @@
 <template>
-    <div>
-        <h1>Sono la Character list</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col" v-for="(character, index) in characters">
+                <div class="card">
+                    <h1>{{ character.name }}</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    name: 'CharactersList'
+    name: 'CharactersList',
+    data() {
+        return {
+            characters: [],
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            currentPage: 1,
+            lastPage: null
+        }
+
+    },
+    methods: {
+        getData(numPage) {
+            axios.get(`${this.apiBaseUrl}/characters`, {
+
+            }).then((res) => {
+                this.characters = res.data.results.data;
+                console.log(this.characters);
+                this.currentPage = res.data.results.current_page;
+                this.lastPage = res.data.results.last_page;
+            });
+
+        }
+    },
+    mounted() {
+        this.getData();
+    }
 }
 </script>
 
