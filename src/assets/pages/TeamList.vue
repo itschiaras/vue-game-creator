dc1006fd8f5e9f9f7d91203efefd55105ae1f8ea
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-16 15:05:49 +0200

    Team

--- src/assets/pages/TeamList.vue
+++ src/assets/pages/TeamList.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="position-absolute"></div>
-    <div class="container">
-        <h1>L'idea se ti garba è dedicare una pagina a noi che abbiamo fatto l'app 🚀</h1>
-        <div class="row gy-3 align-items-center justify-content-center row-cols-lg-5">
+    <div class="container d-flex flex-column h-100 justify-content-start">
+        <div class="row title">
+            <h1 class="text-center fw-bold ">Our Team &hearts;</h1>
+        </div>
+        <div class="row align-items-center justify-content-center row-cols-lg-5">
             <TeamCard v-for="(member, index) in members" :member="member" :key="index" />
         </div>
     </div>
@@ -20,26 +21,46 @@ export default {
             members: [
                 {
                     name: 'Chiara Sucato',
-                    description: 'FRONT-MEN Junior Full Stack Developer',
+                    image: 'src/assets/img/photo_2023-06-16_12-21-25.jpg',
+                    githubURL: 'https://github.com/itschiaras',
+                    ig: 'https://www.instagram.com/itschiaraesse/',
+                    fb: 'https://www.facebook.com/chiara.sucato.5/',
+                    description: 'Junior Full Stack Developer',
                     technologies: ['HTML', 'CSS', 'JS', 'VUE', 'PHP', 'LARAVEL']
                 },
                 {
                     name: 'Marco Bione',
+                    image: 'src/assets/img/121133430.jpg',
+                    githubURL: 'https://github.com/MarcoBione',
+                    ig: null,
+                    fb: null,
                     description: 'Junior Full Stack Developer',
                     technologies: ['HTML', 'CSS', 'JS', 'VUE', 'PHP', 'LARAVEL']
                 },
                 {
                     name: 'Gaetano Spampinato',
+                    image: 'src/assets/img/photo_2023-06-16_12-29-10.jpg',
+                    githubURL: 'https://github.com/Gaetano26',
+                    ig: null,
+                    fb: null,
                     description: 'Junior Full Stack Developer',
                     technologies: ['HTML', 'CSS', 'JS', 'VUE', 'PHP', 'LARAVEL']
                 },
                 {
                     name: 'Andrea Collura',
+                    image: 'src/assets/img/photo_2023-06-16_12-16-30.jpg',
+                    githubURL: 'https://github.com/AndreaCollura',
+                    ig: null,
+                    fb: null,
                     description: 'Junior Full Stack Developer',
                     technologies: ['HTML', 'CSS', 'JS', 'VUE', 'PHP', 'LARAVEL']
                 },
                 {
                     name: 'Marika Di Blasio',
+                    image: 'src/assets/img/Foto_Marika_Di_Blasio.png',
+                    githubURL: 'https://github.com/marikadiblasio',
+                    ig: null,
+                    fb: null,
                     description: 'Junior Full Stack Developer',
                     technologies: ['HTML', 'CSS', 'JS', 'VUE', 'PHP', 'LARAVEL']
                 }
@@ -57,4 +78,10 @@ export default {
     right: 0;
     left: 0;
 }
+
+.container {
+    padding-top: 100px;
+}
+
+
 </style>
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,17 +1,22 @@
 <template>
-        <div class="col pt-3 ">
+        <div class="col pt-3 text-center ">
             <div class="my-card border p-3 text-white">
-                <!-- <img src="..." class="card-img-top" alt="..."> -->
-                <div class="">
+                <img :src="member.image" class="card-img-top rounded-circle" alt="...">
+                <div class="mt-2">
                     <h5 class="">{{member.name}}</h5>
                     <h6>
                         {{member.description}}
                     </h6>
                 </div>
-                <h6>Che cosa ha utilizzato</h6>
-                <ul class="list-unstyled small d-flex flex-wrap">
+                <h6>Tecnologie utilizzate</h6>
+                <ul class="list-unstyled small d-flex flex-wrap mb-0">
                     <li v-for="tec in member.technologies" class="border-0 pb-0 col-6"><span >{{ tec }}</span></li>
                 </ul>
+                <div class="d-flex justify-content-center gap-3 mt-2">
+                    <a :href="member.githubURL"><i class="fa-brands fa-github"></i></a>
+                    <a :href="member.ig"><i class="fa-brands fa-instagram"></i></a>
+                    <a :href="member.fb"><i class="fa-brands fa-facebook"></i></a>
+                </div>
             </div>
         </div>
 
@@ -31,7 +36,14 @@ export default {
         color: white;
         border-radius: 15px;
         box-shadow: 5px 6px 25px -3px rgba(255,255,255,0.1);
-        height: 250px;
+        height: 360px;
+        max-width: 250px;
+
+        img {
+            object-fit: cover;
+            width: 150px;
+            height: 150px;
+        }
     }
 
 </style>
